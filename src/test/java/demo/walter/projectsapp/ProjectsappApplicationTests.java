d1bde491acd64289a7c0e842a33fd8dadc8a15ac
walteribanez555 <walteribanez555@gmail.com>
2024-11-14 11:51:22 -0400

    Updated project testing demo

--- src/test/java/demo/walter/projectsapp/ProjectsappApplicationTests.java
+++ src/test/java/demo/walter/projectsapp/ProjectsappApplicationTests.java
@@ -3,7 +3,7 @@ package demo.walter.projectsapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectsappApplication.class)
 class ProjectsappApplicationTests {
 
 	@Test
