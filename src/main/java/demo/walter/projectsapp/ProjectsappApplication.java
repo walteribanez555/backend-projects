135beeee8b5f0cc2f63d15fbfc62b1993abf865d
walteribanez555 <walteribanez555@gmail.com>
2024-11-14 01:05:39 -0400

    Template Project Springboot

---
+++ src/main/java/demo/walter/projectsapp/ProjectsappApplication.java
@@ -0,0 +1,13 @@
+package demo.walter.projectsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/walter/projectsapp/ProjectsappApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.walter.projectsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
